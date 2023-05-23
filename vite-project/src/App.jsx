a64bd5f99d09552118ddbd9ee55364b13ca77641
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-23 20:14:07 -0300

    :sparkles: Atualizando o App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div className="app">
       <div className="header">
         <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário para avaliar o produto</p>
       </div>
     </div>
       
