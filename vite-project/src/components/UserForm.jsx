c57985f889848b50b41ec075b21078dbdf0d58a1
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-30 19:50:27 -0300

    :sparkles: Atualizando pasta components UserForm.jsx

--- vite-project/src/components/UserForm.jsx
+++ vite-project/src/components/UserForm.jsx
@@ -7,9 +7,20 @@ const UserForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input 
                     type="text" 
-                    name="text"
+                    name="name"
                     id="name" 
                     placeholder="Digite o seu nome" 
+                    required 
+                />
+            </div>
+
+            <div className="form-control">
+                <label htmlFor="email">E-mail:</label>
+                <input 
+                    type="email" 
+                    name="email"
+                    id="email" 
+                    placeholder="Digite o seu e-mail" 
                     required 
                 />
             </div>
