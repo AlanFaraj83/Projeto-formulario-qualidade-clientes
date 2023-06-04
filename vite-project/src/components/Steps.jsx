d04ce6c17028a05c45c2a996f5edfaf2d7078283
AlanFaraj83 <alanfaraj@outlook.com>
2023-06-03 21:34:52 -0300

    :sparkles:Atualizando pasta components Steps.jsx

--- vite-project/src/components/Steps.jsx
+++ vite-project/src/components/Steps.jsx
@@ -1,16 +1,21 @@
-import "./Steps.css"
+import {AiOutLineUser, AiOutLineStar} from 'react-icons/ai';
+import { FiSend } from 'react-icons/fi';
+import "./Steps.css";
 
 const Steps = ({ currentStep }) => {
     return <div className="steps">
         <div className="step">
+            <AiOutLineUser />
             <p>Identificação</p>
         </div>
 
         <div className="step">
+            <AiOutLineStar />
             <p>Avaliação</p>
         </div>
 
         <div className="step">
+            <FiSend />
             <p>Envio</p>
         </div>
     </div>;
