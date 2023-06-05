4a47de234c8328d1bd32ccaed174ceb2fa4cc579
AlanFaraj83 <alanfaraj@outlook.com>
2023-06-05 20:09:41 -0300

    :sparkles: Atualizando pasta components ReviewForm.jsx

--- vite-project/src/components/ReviewForm.jsx
+++ vite-project/src/components/ReviewForm.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const ReviewForm = () => {
     return (
         <div className="review-form">
-            <h2>Review Form</h2>
+            <div className="form-control score-container">
+                <label className="radio-container">
+                    
+                </label>
+            </div>
         </div>
     )
 }
