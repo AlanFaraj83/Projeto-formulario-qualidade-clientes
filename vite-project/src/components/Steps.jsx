006f0534232ebd3b68e0bc0bf514b0c768b15db4
AlanFaraj83 <alanfaraj@outlook.com>
2023-06-04 21:06:10 -0300

    :sparkles:Atualizando pasta components Steps.jsx

--- vite-project/src/components/Steps.jsx
+++ vite-project/src/components/Steps.jsx
@@ -8,12 +8,10 @@ const Steps = ({ currentStep }) => {
             <AiOutlineUser />
             <p>Identificação</p>
         </div>
-
         <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
             <AiOutlineStar />
             <p>Avaliação</p>
         </div>
-
         <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
             <FiSend />
             <p>Envio</p>
