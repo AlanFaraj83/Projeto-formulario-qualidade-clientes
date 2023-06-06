6cd1b58479fceb1e9d56b8accf3ec8a277c0989b
AlanFaraj83 <alanfaraj@outlook.com>
2023-06-06 18:31:27 -0300

    :sparkles: Atualizando pasta components ReviewForm.jsx

--- vite-project/src/components/ReviewForm.jsx
+++ vite-project/src/components/ReviewForm.jsx
@@ -11,7 +11,26 @@ const ReviewForm = () => {
             <div className="form-control score-container">
                 <label className="radio-container">
                     <input type="radio" value="unsatisfied" name="review" required/>
+                    <BsFillEmojiFrownFill />
                     <p>Insatisfeito</p>
+                </label>
+
+                <label className="radio-container">
+                    <input type="radio" value="neutral" name="review" required/>
+                    <BsFillEmojiNeutralFill />
+                    <p>Poderia ser melhor</p>
+                </label>
+
+                <label className="radio-container">
+                    <input type="radio" value="satisfied" name="review" required/>
+                    <BsFillEmojiSmileFill />
+                    <p>Satisfeito</p>
+                </label>
+
+                <label className="radio-container">
+                    <input type="radio" value="very_satisfied" name="review" required/>
+                    <BsFillEmojiHeartEyesFill />
+                    <p>Muito Satisfeito</p>
                 </label>
             </div>
         </div>
