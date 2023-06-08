07a78ab5ef4d2e7acc34add05af2de80c8f21b71
AlanFaraj83 <alanfaraj@outlook.com>
2023-06-08 00:43:52 -0300

    :sparkles: Atualizando pasta components Thanks.jsx

--- vite-project/src/components/Thanks.jsx
+++ vite-project/src/components/Thanks.jsx
@@ -14,6 +14,9 @@ const Thanks = () => {
             <p>A sua opinião é importante, em breve você receberá um cupom de 10% de desconto para sua próxima compra.</p>
             <p>Para concluir a avaliação clique no botão de Enviar abaixo.</p>
             <h3>Aqui está o resumo de sua avaliação:</h3>
+            <p className="review-data">
+                <span>Satisfação com o produto:</span>
+            </p>
         </div>
     )
 }
