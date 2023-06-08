8f0afa52169f3f64c0c2f031cb19176d680d88e1
AlanFaraj83 <alanfaraj@outlook.com>
2023-06-08 00:12:28 -0300

    :sparkles: Atualizando pasta components Thanks.jsx

--- vite-project/src/components/Thanks.jsx
+++ vite-project/src/components/Thanks.jsx
@@ -9,8 +9,9 @@ import "./Thanks.css";
 
 const Thanks = () => {
     return (
-        <div>
-            <h2>Thanks</h2>
+        <div className='thanks-container'>
+            <h2>Falta agora pouco...</h2>
+            <p>A sua opinião é importante, em breve você receberá um cupom de 10% de desconto para sua próxima compra.</p>
         </div>
     )
 }
